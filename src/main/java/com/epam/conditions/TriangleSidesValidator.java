af7c652f16d2f4b69714906083733e5f9e0194b1
Dmitry Golub <dmgolub@mail.ru>
2022-12-26 01:49:32 +0400

    Master task completed.

--- src/main/java/com/epam/conditions/TriangleSidesValidator.java
+++ src/main/java/com/epam/conditions/TriangleSidesValidator.java
@@ -3,7 +3,15 @@ package com.epam.conditions;
 public class TriangleSidesValidator {
 
     public void validate(double firstSide, double secondSide, double thirdSide) {
-
+        if (isTriangle(firstSide, secondSide, thirdSide)) {
+            System.out.println("this is a valid triangle");
+        } else {
+            System.out.println("it's not a triangle");
+        }
     }
 
+    private boolean isTriangle(double firstSide, double secondSide, double thirdSide) {
+        return firstSide + secondSide > thirdSide && firstSide + thirdSide > secondSide
+                && secondSide + thirdSide > firstSide;
+    }
 }
--- src/main/java/com/epam/loops/Pyramid.java
+++ src/main/java/com/epam/loops/Pyramid.java
@@ -3,11 +3,21 @@ package com.epam.loops;
 public class Pyramid {
 
     public void printPyramid(int cathetusLength) {
-
+        for (int rowNumber = 0; rowNumber < cathetusLength; rowNumber++) {
+            for (int j = 0; j < cathetusLength + rowNumber; j++) {
+                if (j >= cathetusLength - rowNumber - 1 && j < cathetusLength) {
+                    System.out.print(cathetusLength - j);
+                } else if ( j >= cathetusLength) {
+                    System.out.print(j - cathetusLength + 2);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
         new Pyramid().printPyramid(7);
     }
-
 }
